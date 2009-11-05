33734906724a0f9bf8c61b4e602a4cacc66951bb
Walter Bender <walter@walter-laptop.(none)>
2009-11-05 17:27:18 -0500

    new project

---
+++ ErikosActivity.py
@@ -0,0 +1,149 @@
+# -*- coding: utf-8 -*-
+#Copyright (c) 2009, Walter Bender
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+
+import pygtk
+pygtk.require('2.0')
+import gtk
+import gobject
+
+import sugar
+from sugar.activity import activity
+try: # 0.86+ toolbar widgets
+    from sugar.bundle.activitybundle import ActivityBundle
+    from sugar.activity.widgets import ActivityToolbarButton
+    from sugar.activity.widgets import StopButton
+    from sugar.graphics.toolbarbox import ToolbarBox
+    from sugar.graphics.toolbarbox import ToolbarButton
+except ImportError:
+    pass
+from sugar.graphics.toolbutton import ToolButton
+from sugar.graphics.menuitem import MenuItem
+from sugar.graphics.icon import Icon
+from sugar.datastore import datastore
+
+from gettext import gettext as _
+import locale
+import os.path
+
+import logging
+_logger = logging.getLogger("erikos-activity")
+
+from sprites import *
+import window
+
+#
+# Sugar activity
+#
+class ErikosActivity(activity.Activity):
+
+    def __init__(self, handle):
+        super(ErikosActivity,self).__init__(handle)
+
+        try:
+            # Use 0.86 toolbar design
+            toolbar_box = ToolbarBox()
+
+            # Buttons added to the Activity toolbar
+            activity_button = ActivityToolbarButton(self)
+            toolbar_box.toolbar.insert(activity_button, 0)
+            activity_button.show()
+
+            # Play Button
+            self.play = ToolButton( "media-playback-start" )
+            self.play.set_tooltip(_('Play'))
+            self.play.props.sensitive = True
+            self.play.connect('clicked', self._play_cb)
+            toolbar_box.toolbar.insert(self.play, -1)
+            self.play.show()
+
+            separator = gtk.SeparatorToolItem()
+            separator.props.draw = False
+            separator.set_expand(True)
+            separator.show()
+            toolbar_box.toolbar.insert(separator, -1)
+
+            # The ever-present Stop Button
+            stop_button = StopButton(self)
+            stop_button.props.accelerator = '<Ctrl>Q'
+            toolbar_box.toolbar.insert(stop_button, -1)
+            stop_button.show()
+
+            self.set_toolbar_box(toolbar_box)
+            toolbar_box.show()
+
+        except NameError:
+            # Use pre-0.86 toolbar design
+            self.toolbox = activity.ActivityToolbox(self)
+            self.set_toolbox(self.toolbox)
+
+            self.projectToolbar = ProjectToolbar(self)
+            self.toolbox.add_toolbar( _('Project'), self.projectToolbar )
+
+            self.toolbox.show()
+
+        # Create a canvas
+        canvas = gtk.DrawingArea()
+        canvas.set_size_request(gtk.gdk.screen_width(), \
+                                gtk.gdk.screen_height())
+        self.set_canvas(canvas)
+        canvas.show()
+        self.show_all()
+
+        # Initialize the canvas
+        self.tw = window.new_window(canvas, \
+                                    os.path.join(activity.get_bundle_path(), \
+                                                 'images/'), \
+                                    self)
+
+        # Read the level from the Journal
+        try:
+            tw,level = int(self.metadata['level'])
+        except:
+            pass
+
+    def _play_cb(self, button):
+        window._play_the_game(self.tw)
+        return True
+
+    """
+    Write the slider positions to the Journal
+    """
+    def write_file(self, file_path):
+        _logger.debug("Write level: " + str(self.tw.level))
+        self.metadata['level'] = self.tw.level
+
+#
+# Project toolbar for pre-0.86 toolbars
+#
+class ProjectToolbar(gtk.Toolbar):
+
+    def __init__(self, pc):
+        gtk.Toolbar.__init__(self)
+        self.activity = pc
+
+        # Play button
+        self.activity.play = ToolButton( "media-playback-start" )
+        self.activity.play.set_tooltip(_('Play'))
+        self.activity.play.props.sensitive = True
+        self.activity.play.connect('clicked', self.activity._play_cb)
+        self.insert(self.activity.play, -1)
+        self.activity.play.show()
+
---
+++ window.py
@@ -0,0 +1,223 @@
+# -*- coding: utf-8 -*-
+#Copyright (c) 2009, Walter Bender
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+
+from constants import *
+from time import *
+import pygtk
+pygtk.require('2.0')
+import gtk
+import gobject
+from sound import playWave, audioWrite
+import os
+
+from gettext import gettext as _
+import math
+
+try:
+    from sugar.graphics import style
+    GRID_CELL_SIZE = style.GRID_CELL_SIZE
+except:
+    GRID_CELL_SIZE = 0
+
+from sprite_factory import *
+
+class srWindow: pass
+
+#
+# handle launch from both within and without of Sugar environment 
+#
+def new_window(canvas, path, parent=None):
+    tw = srWindow()
+    tw.path = path
+    tw.activity = parent
+
+    # starting from command line
+    # we have to do all the work that was done in CardSortActivity.py
+    if parent is None:
+        tw.sugar = False
+        tw.canvas = canvas
+
+    # starting from Sugar
+    else:
+        tw.sugar = True
+        tw.canvas = canvas
+        parent.show_all()
+
+    tw.canvas.set_flags(gtk.CAN_FOCUS)
+    tw.canvas.add_events(gtk.gdk.BUTTON_PRESS_MASK)
+    tw.canvas.add_events(gtk.gdk.BUTTON_RELEASE_MASK)
+    tw.canvas.add_events(gtk.gdk.POINTER_MOTION_MASK)
+    tw.canvas.connect("expose-event", _expose_cb, tw)
+    tw.canvas.connect("button-press-event", _button_press_cb, tw)
+    tw.canvas.connect("button-release-event", _button_release_cb, tw)
+    tw.canvas.connect("motion-notify-event", _mouse_move_cb, tw)
+    tw.width = gtk.gdk.screen_width()
+    tw.height = gtk.gdk.screen_height()-GRID_CELL_SIZE
+    tw.area = tw.canvas.window
+    tw.gc = tw.area.new_gc()
+    tw.cm = tw.gc.get_colormap()
+    tw.msgcolor = tw.cm.alloc_color('black')
+    tw.sprites = []
+    tw.scale = 1
+    tw.level = 0
+    tw.seq = [[0,1],[0,1,0,1],[0,1,2,3,0,1,2,3],[0,1,2,3,0,1,2,3,0,1,2,3]]
+    tw.counter = 0
+
+    # Open the sliders
+    tw.buttons_off = [Sprite(tw,"Aoff",tw.width/2-W/2,H/2,W/2,H/2),\
+                      Sprite(tw,"Boff",tw.width/2-W,H,W/2,H/2),\
+                      Sprite(tw,"Doff",tw.width/2,H,W/2,H/2),\
+                      Sprite(tw,"Coff",tw.width/2-W/2,H+H/2,W/2,H/2)]
+    tw.buttons_on  = [Sprite(tw,"Aon",tw.width/2-W/2,H/2,W/2,H/2),\
+                      Sprite(tw,"Bon",tw.width/2-W,H,W/2,H/2),\
+                      Sprite(tw,"Don",tw.width/2,H,W/2,H/2),\
+                      Sprite(tw,"Con",tw.width/2-W/2,H+H/2,W/2,H/2)]
+    tw.sounds = ['dog','sheep','cat','bird']
+    tw.sound_files = []
+
+    for i in tw.sounds:
+        playWave(i)
+        path = tw.activity.get_activity_root() + "/instance/" + i + ".csd"
+        tw.sound_files.append(path)
+        audioWrite(path)
+
+    _all_off(tw)
+
+    # Start calculating
+    tw.press = None
+    tw.dragpos = 0
+    return tw
+
+#
+# Play
+#
+def _play_the_game(tw):
+    _all_on(tw)
+    for i in tw.buttons_on:
+        hide(i.spr)
+    for i in tw.seq[tw.level]:
+        draw(tw.buttons_on[i].spr)
+        inval(tw.buttons_on[i].spr)
+        # need to learn how timeouts really work...
+        print tw.sound_files[i]
+        os.system('csound ' + tw.sound_files[i]  + \
+                  '/temp.csd >/dev/null 2>/dev/null')
+        sleep(1)
+        gobject.timeout_add(1000,hide,tw.buttons_on[i].spr)
+        # hide(tw.buttons_on[i].spr)
+    tw.counter = 0
+
+#
+# Button press
+#
+def _button_press_cb(win, event, tw):
+    win.grab_focus()
+    x, y = map(int, event.get_coords())
+    tw.dragpos = x
+    spr = findsprite(tw,(x,y))
+    tw.press = spr
+    return True
+
+#
+# Mouse move
+#
+def _mouse_move_cb(win, event, tw):
+    if tw.press is None:
+        tw.dragpos = 0
+        return True
+
+    win.grab_focus()
+    x, y = map(int, event.get_coords())
+
+    dx = x-tw.dragpos
+
+    # reset drag position
+    tw.dragpos = x
+
+#
+# Button release
+#
+def _button_release_cb(win, event, tw):
+    if tw.press == None:
+        return True
+    for i in range (0,4):
+        if tw.press == tw.buttons_off[i].spr:
+            draw(tw.buttons_on[i].spr)
+            inval(tw.buttons_on[i].spr)
+            # gobject.timeout_add(1000,hide,tw.buttons_on[i].spr)
+            print tw.sound_files[i]
+            os.system('csound ' + tw.sound_files[i])
+            # os.system('csound ' + tw.sound_files[i] + \
+            #          '/temp.csd >/dev/null 2>/dev/null')
+            sleep(1)
+            hide(tw.buttons_on[i].spr)
+            if tw.seq[tw.level][tw.counter] == i: # correct reponse
+                """
+                print str(tw.counter) + ": made a match between " + \
+                      str(i) + " and " + str(tw.seq[tw.counter])
+                """
+                tw.counter += 1
+                if tw.counter == len(tw.seq[tw.level]):
+                    _all_on(tw)
+                    tw.counter = 0
+                    tw.level += 1
+                    if tw.level == len(tw.seq):
+                        tw.level = 0
+                    sleep(1)
+                    _all_off(tw)
+            else: # incorrect response
+                """
+                print "mismatch: " + str(i) + " is not " + \
+                      str(tw.seq[tw.counter])
+                """
+
+                """
+                _all_gone(tw)
+                sleep(1)
+                _all_off(tw)
+                """
+                tw.counter = 0
+    tw.press = None
+
+def _all_on(tw):
+    for i in tw.buttons_off:
+        i.draw_slider()
+    for i in tw.buttons_on:
+        i.draw_slider()
+
+def _all_off(tw):
+    for i in tw.buttons_off:
+        i.draw_slider()
+    for i in tw.buttons_on:
+        hide(i.spr)
+
+def _all_gone(tw):
+    for i in tw.buttons_off:
+        hide(i.spr)
+    for i in tw.buttons_on:
+        hide(i.spr)
+
+def _expose_cb(win, event, tw):
+    redrawsprites(tw)
+    return True
+
+def _destroy_cb(win, event, tw):
+    gtk.main_quit()
